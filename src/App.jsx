8fdb944cc2bc161dd99fb07fa41482b1308fce68
eva.wang <eva.wang81@gmail.com>
2024-05-22 12:55:44 +0800

    added post new word function

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,54 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { NewWordForm } from "../NewWordForm";
 import { WordList } from "../WordList";
+import axios from "axios";
 
 export default function App() {
-  const [wordList, setWordList] = useState([]);
+  const [wordList, setWordList] = useState(() => {
+    const localValue = localStorage.getItem("WORDS");
+    if (localValue === null) return [];
+    return JSON.parse(localValue);
+  });
 
-  function addWord(newWord) {
-    setWordList((currentWordList) => {
-      return [
-        ...currentWordList,
-        { id: crypto.randomUUID(), word: newWord, mastered: false },
-      ];
-    });
+  const url = import.meta.env.VITE_BACKEND_URL;
+
+  useEffect(() => {
+    localStorage.setItem("WORDS", JSON.stringify(wordList));
+
+    async function getWordsData() {
+      try {
+        // const { data } = await axios.get(`${url}/allwords`);
+        // console.log("data: ", data);
+      } catch (err) {
+        console.log("msg: ", err);
+      }
+    }
+
+    getWordsData();
+  }, [wordList]);
+
+  async function addWord(newWord) {
+    console.log("new word: ", newWord);
+
+    try {
+      const addNewWord = await axios.post(`${url}/new`, {
+        newWord: newWord,
+      });
+
+      const data = addNewWord.data;
+      console.log(data);
+      if (data.newWordAdded) {
+        const newWord = { id: data.id, word: data.newWord, mastered: false };
+
+        setWordList((currentWordList) => {
+          return [...currentWordList, newWord];
+        });
+      }
+
+      if (data.isExistingWord) alert("Word already added!");
+    } catch (err) {
+      console.log("msg: ", err);
+    }
   }
 
   function toggleWordState(id, mastered) {
