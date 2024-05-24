d86643686586049ee71c0550a0ae325eb2bd8e37
eva.wang <eva.wang81@gmail.com>
2024-05-24 16:11:20 +0800

    added wordreference api call to get translation fr-en

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,21 @@
+// var wr = require("wordreference-api");
+import { defineWord } from "wordreference";
+import { useState, useEffect } from "react";
+import Definition from "./Definition";
+
 export function Card({ is_mastered, id, word, toggleWordState, deleteWord }) {
+  const [definition, setDefinition] = useState([]);
+  const [audioLink, setAudioLink] = useState("");
+
+  async function showDefinition() {
+    const definition = await defineWord(word, "French-English");
+    setDefinition(definition.sections);
+    setAudioLink(definition.audioLinks[0]);
+    console.log(definition);
+  }
+
+  console.log(definition, audioLink);
+
   return (
     <li>
       {/* <label htmlFor="wordState"> */}
@@ -11,6 +28,19 @@ export function Card({ is_mastered, id, word, toggleWordState, deleteWord }) {
         />
         {word}
       </label>
+      <button className="btn btn-prime" onClick={() => showDefinition()}>
+        View
+      </button>
+      <div>
+        {definition.length > 0 && (
+          <Definition
+            definition={definition}
+            audioLink={audioLink}
+            word={word}
+          />
+        )}
+      </div>
+
       <button className="btn btn-danger" onClick={() => deleteWord(id)}>
         Remove
       </button>
