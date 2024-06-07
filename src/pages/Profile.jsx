d2512ac8903d7139530107d790377585bf7f7e0a
eva.wang <eva.wang81@gmail.com>
2024-06-07 15:54:14 +0800

    updated git ignore

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -9,6 +9,8 @@ import Divider from "@mui/material/Divider";
 import Loader from "../components/Loader";
 import { useNavigate } from "react-router-dom";
 import { Margin, Padding, Widgets, WidthFull } from "@mui/icons-material";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import Button from "@mui/material/Button";
 
 export default function Profile() {
   const url = import.meta.env.VITE_BACKEND_URL;
@@ -25,8 +27,11 @@ export default function Profile() {
           const getUserData = await axios.get(
             `${url}/users/${currentUser.userId}`
           );
-          // console.log(getUserData);
-          setProfile(getUserData.data.user);
+          if (!getUser) {
+            navigate("/");
+          } else {
+            setProfile(getUserData.data.user);
+          }
         } catch (err) {
           console.log(err);
         }
@@ -36,7 +41,13 @@ export default function Profile() {
     }
   }, []);
 
-  console.log(profile);
+  const handleDeleteAccount = async () => {
+    const callDelete = await axios.put(
+      `${url}/users/${currentUser.userId}/deactivate`
+    );
+    navigate("/");
+    localStorage.clear();
+  };
 
   return (
     <Box
@@ -44,35 +55,103 @@ export default function Profile() {
         height: "100%",
         display: "flex",
         flexDirection: "column",
+        justifyContent: "center",
         alignItems: "center",
         mx: 4,
         marginTop: "50px",
       }}
     >
-      <h4>My profile page</h4>
-      <Divider sx={{ border: "0.5px solid gray", width: "100%", my: 0.5 }} />
+      <Box>
+        <AccountCircleIcon sx={{ fontSize: 60 }} />
+      </Box>
+      <Divider sx={{ width: "100%", my: 2 }} />
 
-      <Box sx={{ width: "100%" }}>
-        <table aria-label="custom pagination table">
-          <tbody>
-            <tr>
-              <td>Display Name: </td>
-              <td> {profile.displayName}</td>
-            </tr>
-            <tr>
-              <td>First Name: </td>
-              <td>{profile.firstName}</td>
-            </tr>
-            <tr>
-              <td>Last Name: </td>
-              <td>{profile.lastName}</td>
-            </tr>
-            <tr>
-              <td>Email: </td>
-              <td>{profile.email}</td>
-            </tr>
-          </tbody>
-        </table>
+      <Box
+        sx={{
+          width: "100%",
+          display: "flex",
+          flexDirection: "row",
+          height: "200px",
+        }}
+      >
+        <Box
+          sx={{
+            width: "35%",
+            height: "40px",
+            my: 1,
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <Box sx={{ minHeight: "20px", my: 1 }}>Display Name:</Box>
+          <Box sx={{ minHeight: "20px", my: 1 }}>First Name:</Box>
+          <Box sx={{ minHeight: "20px", my: 1 }}>Last Name: </Box>
+          <Box sx={{ minHeight: "20px", my: 1 }}>Email:</Box>
+        </Box>
+        <Box
+          sx={{
+            width: "65%",
+            height: "40px",
+            my: 1,
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <Box
+            sx={{
+              minHeight: "20px",
+              m: 1,
+              borderBottom: "1px solid gray",
+              px: 2,
+            }}
+          >
+            {profile.displayName}
+          </Box>
+          <Box
+            sx={{
+              minHeight: "20px",
+              m: 1,
+              borderBottom: "1px solid gray",
+              px: 2,
+            }}
+          >
+            {profile.firstName}
+          </Box>
+          <Box
+            sx={{
+              minHeight: "20px",
+              m: 1,
+              borderBottom: "1px solid gray",
+              px: 2,
+            }}
+          >
+            {profile.lastName}
+          </Box>
+          <Box
+            sx={{
+              minHeight: "20px",
+              m: 1,
+              borderBottom: "1px solid gray",
+              px: 2,
+            }}
+          >
+            {profile.email}
+          </Box>
+        </Box>
+      </Box>
+      <Box sx={{ display: "flex", flexDirection: "row", width: "100%" }}>
+        <Button
+          variant="contained"
+          fullWidth
+          sx={{ mx: 1 }}
+          color="error"
+          onClick={() => handleDeleteAccount()}
+        >
+          Deactivate
+        </Button>
+        <Button variant="contained" fullWidth sx={{ mx: 1 }} color="primary">
+          Edit
+        </Button>
       </Box>
       <BottomNav />
     </Box>
