ef4e1f42fe58d313af1499d02a747d92a6710dd6
eva.wang <eva.wang81@gmail.com>
2024-06-27 09:10:26 +0800

    clear up dashboard fun

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,28 +1,17 @@
 import Box from "@mui/material/Box";
 import BottomNav from "../components/BottomNav";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
-import { CardActionArea } from "@mui/material";
 import WordChart from "../components/WordChart";
 import { useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useEffect } from "react";
 
 export default function Dashboard() {
   const currentUser = JSON.parse(localStorage.getItem("user"));
   const navigate = useNavigate();
-  const [totalWords, setTotalWords] = useState(0);
-  const [masteredWords, setMasteredWords] = useState(0);
 
   useEffect(() => {
     if (!currentUser) {
       navigate("/login");
     }
-
-    const getData = async () => {
-      await axios.get(`url/`);
-    };
   }, []);
 
   return (
