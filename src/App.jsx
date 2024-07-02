58483a1be7a349264819ad14e9f0a1de07622a1b
eva.wang <eva.wang81@gmail.com>
2024-07-02 15:50:32 +0800

    removed width breakpoint

--- src/App.jsx
+++ src/App.jsx
@@ -11,31 +11,30 @@ import Profile from "./pages/Profile";
 
 export default function App() {
   const [width, setWidth] = useState(window.innerWidth);
-  const breakpoint = 2620;
+  const breakpoint = 620;
   axios.defaults.withCredentials = true;
 
-  useEffect(() => {
-    window.addEventListener("resize", () => setWidth(window.innerWidth));
-  }, []);
+  // useEffect(() => {
+  //   window.addEventListener("resize", () => setWidth(window.innerWidth));
+  // }, []);
 
   return (
-    // <Provider config={rollbarConfig}>
     <>
-      {width < breakpoint ? (
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/register" element={<Register />} />
-          <Route exact path="/dashboard" element={<Dashboard />} />
-          <Route exact path="/new" element={<New />} />
-          <Route exact path="/words" element={<Words />} />
-          <Route exact path="/profile" element={<Profile />} />
-        </Routes>
-      ) : (
+      {/* {width < breakpoint ? ( */}
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/register" element={<Register />} />
+        <Route exact path="/dashboard" element={<Dashboard />} />
+        <Route exact path="/new" element={<New />} />
+        <Route exact path="/words" element={<Words />} />
+        <Route exact path="/profile" element={<Profile />} />
+      </Routes>
+      {/* ) : (
         <div style={{ width: "100%", height: "100%", marginTop: "200px" }}>
           <h3 style={{ width: "100%" }}>Desktop version coming soon...</h3>
         </div>
-      )}
+      )} */}
     </>
   );
 }
